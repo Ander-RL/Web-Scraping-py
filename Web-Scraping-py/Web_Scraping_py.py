4adc557cff31b68d2a47f66eaa56c9094a6e85dd
Ander-RL <anderrodriguez205@gmail.com>
2021-02-11 12:42:55 +0100

    Connection to db and queries Ok.

--- Web-Scraping-py/Web_Scraping_py.py
+++ Web-Scraping-py/Web_Scraping_py.py
@@ -1,47 +1,100 @@
 from bs4 import BeautifulSoup
 import urllib.request, urllib.parse, urllib.error
 import json
+import MySQLdb
+import mysql.connector
+from MySQLdb import _mysql
 from LinksReader import LinksReader
+from datetime import datetime
 
-link_reader = LinksReader()
-link_list = link_reader.get_link_list()
-print(link_list)
-print("\n=========================================================================\n")
+print("Connecting to database...")
+# Data to connecto to DB
+HOST = "localhost"
+DATABASE = "web_scraper"
+TABLE = "product"
+USER = "root"
+PASSWORD = "root"
 
-for link in link_list:
-    url = link
-    print(url)
-    # Open the URL as Browser, not as python urllib to avoid Forbidden errors
-    page = urllib.request.Request(url,headers={'User-Agent': 'Chrome'}) 
-    html = urllib.request.urlopen(page).read()
+# Todays date
+today = datetime.today().strftime("%Y-%m-%d")
 
-    soup = BeautifulSoup(html, features="lxml") # lxml helps to interpret the html
+try:
+    # Connection to DB
+    #conn = _mysql.connect(HOST, USER, PASSWORD, DATABASE)
+    conn = mysql.connector.connect(host = HOST, user = USER, password = PASSWORD, database = DATABASE)
+    my_cursor = conn.cursor()
+    print("Connection succesful!")
 
-    tag_sections = soup.find_all("div", class_="precioMain h1")
-    for section in tag_sections:
-        print(section)
-
-    print("\nPrecio:",tag_sections[0].text)
-
-    disponible = False
-
-    try:
-        #tag_comprar = soup.find("button", class_="btn js-article-buy btn-primary btn-lg buy GTM-addToCart buy-button")
-        tag_comprar = soup.find_all("button", type="button")
-
-        for element in tag_comprar:
-            if "Comprar" in element.text:
-                disponible = True
-            if "Avísame" in element.text:
-                disponible = False
-
-        if disponible:
-           print("Disponible")
-
-        if not disponible:
-           print("No disponible")
-
-    except Exception as e:
-            print(e)
-
+    link_reader = LinksReader()
+    link_list = link_reader.get_link_list()
+    #print(link_list)
     print("\n=========================================================================\n")
+
+    # This code is for products of www.pccomponentes.com
+    db_list = list()
+
+    for link in link_list:
+        url = link
+        print(url)
+        # Open the URL as Browser, not as python urllib to avoid Forbidden errors
+        page = urllib.request.Request(url,headers={'User-Agent': 'Chrome'}) 
+        html = urllib.request.urlopen(page).read()
+
+        soup = BeautifulSoup(html, features="lxml") # lxml helps to interpret the html
+
+        tag_sections = soup.find_all("div", class_="precioMain h1")
+        tag_item_name = soup.find("div", class_="articulo")
+
+        name_product = tag_item_name.text
+        print(tag_item_name.text)
+
+        price_product = tag_sections[0].text
+        price_product = price_product.replace("€", " E")
+        print("\nPrecio:",tag_sections[0].text)
+
+        available = False
+
+        try:
+            tag_buy = soup.find_all("button", type="button")
+
+            for element in tag_buy:
+                if "Comprar" in element.text:
+                    available = True
+                if "Avísame" in element.text:
+                    available = False
+
+            if available:
+                available = "Available"
+                print("Available")
+
+            if not available:
+                available = "Not available"
+                print("Not available")
+
+            # Select query to check if already in table
+            select_query = f"SELECT url_product FROM {TABLE} WHERE url_product = \"{url}\""
+            my_cursor.execute(select_query)
+
+            for link in my_cursor:
+                db_list.append(link[0]) # Each element in the cursor is a list
+
+            if url not in db_list:
+                # Insert query
+                insert_query = f"INSERT INTO product VALUES (0, \"{name_product}\", \"{price_product}\", \"{today}\", \"{url}\", \"{available}\")"
+                my_cursor.execute(insert_query)
+                conn.commit()
+
+        except Exception as e:
+                print(e)
+
+        print("\n=========================================================================\n")
+
+    print("Connection to database closed.")
+    conn.close()
+
+except Exception as e:
+    print(e)
+
+# Insert codes for database connection into a function.
+# Insert each query lines into a function for each store.
+# Write function that allows user to choose store to scrap.
